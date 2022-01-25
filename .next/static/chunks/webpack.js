b744a93ea467cb06a8cc83581707bd7571e5f346
GuilhermeCostaDF <guido.costa@globo.com>
2022-01-25 15:36:33 -0300

    Estilizando a primeira pagina

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e2681b919579b96"; }
+/******/ 		__webpack_require__.h = function() { return "26b0379cdf74d6a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.64fc010f2b8acf88.hot-update.js
+++ .next/static/webpack/webpack.64fc010f2b8acf88.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "69dbe7dd2b9f66ef"; }
+/******/ 	__webpack_require__.h = function() { return "0eb039a44d834787"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/harmony module decorator */
